f1fc43d07faa4ee6e76e68148108176b4b80300d
itz-TS <trishashanvi01@gmail.com>
2026-04-08 15:12:48 +0530

    previous correct test

--- script.test.js
+++ script.test.js
@@ -1,18 +1,18 @@
-// const { isValidTask } = require("./script");
+const { isValidTask } = require("./script");
 
-// test("should not allow empty task", () => {
-//     expect(isValidTask("")).toBe(false);
-// });
-
-// test("should not allow only spaces", () => {
-//     expect(isValidTask("   ")).toBe(false);
-// });
-
-// test("should allow valid task", () => {
-//     expect(isValidTask("Buy groceries")).toBe(true);
-// });
-
-
-test("intentional fail test", () => {
-    expect(1 + 1).toBe(2);
+test("should not allow empty task", () => {
+    expect(isValidTask("")).toBe(false);
 });
+
+test("should not allow only spaces", () => {
+    expect(isValidTask("   ")).toBe(false);
+});
+
+test("should allow valid task", () => {
+    expect(isValidTask("Buy groceries")).toBe(true);
+});
+
+
+// test("intentional fail test", () => {
+//     expect(1 + 1).toBe(3);
+// });
